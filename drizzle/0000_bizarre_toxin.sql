57a3678dab913e95c81e0b3c4bf07fa69c1e52ed
shersingh <luckyshera95@gmail.com>
2026-05-07 10:14:24 +0530

    added migrations

--- drizzle/0000_bizarre_toxin.sql
+++ drizzle/0000_bizarre_toxin.sql
@@ -1,7 +1,12 @@
 CREATE TABLE "users" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"name" varchar(255) NOT NULL,
-	"age" integer NOT NULL,
 	"email" varchar(255) NOT NULL,
+	"password" varchar(16) NOT NULL,
+	"active" boolean DEFAULT false NOT NULL,
+	"isDealer" boolean DEFAULT false NOT NULL,
+	"isAdmin" boolean DEFAULT false NOT NULL,
+	"lastLogin" timestamp NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
---
+++ drizzle/0001_quiet_the_liberteens.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "users" ALTER COLUMN "lastLogin" DROP NOT NULL;
